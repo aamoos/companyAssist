26079c712ec5e285e5c7212fd1a34f9e974ae8bc
kimjaesung <aamoos@naver.com>
2024-10-07 16:02:00 +0900

    Initial commit

---
+++ src/main/kotlin/com/companyAssist/CompanyAssistApplication.kt
@@ -0,0 +1,11 @@
+package com.companyAssist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompanyAssistApplication
+
+fun main(args: Array<String>) {
+	runApplication<CompanyAssistApplication>(*args)
+}
---
+++ src/test/kotlin/com/companyAssist/CompanyAssistApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.companyAssist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CompanyAssistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
